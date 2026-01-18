ad0839ad3960163d340d53c9262e76786540eb55
LAWI MOHAMMED GALKAYE <119644685+LGlawi5@users.noreply.github.com>
2026-01-18 18:10:54 +0100

    menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menuToggle");
     const navLinks = document.getElementById("navLinks");
 
+    // Toggle menu on click
     menuToggle.addEventListener("click", function () {
         navLinks.classList.toggle("show");
+    });
+
+    // Close menu on scroll
+    window.addEventListener("scroll", function () {
+        if (navLinks.classList.contains("show")) {
+            navLinks.classList.remove("show");
+        }
     });
 });
